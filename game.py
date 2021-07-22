75dad9d54c8cdbec53914787008fb618b4cf91f6
Israel Sagesse <israelsagesse@gmail.com>
2021-07-21 22:43:59 -0400

    initial commit

---
+++ game.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from random import randint
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Guess a number between 0 and 9</h1>' \
+           '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" alt="guess gift">'
+
+@app.route('/<int:user_guess>')
+def guess(user_guess):
+    correct_number = randint(0, 9)
+    print((correct_number))
+    if user_guess > correct_number:
+        return '<h1 style="color: red">Too high, try again!</h1>' \
+        '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif" >'
+    if user_guess < correct_number:
+        return '<h1 style="color: purple">Too low, try again</h1>' \
+        '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif" >'
+    if user_guess == correct_number:
+        return '<h1 style="color: green">You found me</h1>' \
+        '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif" >'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
